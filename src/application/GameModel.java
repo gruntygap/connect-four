56a7b296128eb095739e76c5ae73a916e814d5bb
Grant Gapinski <gruntygap@gmail.com>
2019-04-29 16:11:40 -0500

    Added more to the isWon method

--- src/application/GameModel.java
+++ src/application/GameModel.java
@@ -9,6 +9,7 @@ public class GameModel {
 	
 	public GameModel(int numRows, int numCols) {
 		configureGrid(numRows, numCols);
+		maxLengthOfWin = 4;
 	}
 	
 	public void configureGrid(int numRows, int numCols) {
@@ -18,6 +19,51 @@ public class GameModel {
 				this.grid[i][j] = 0;
 			}
 		}
+	}
+	
+	public boolean boardFull() {
+		for(int i = 0; i < this.grid.length; i++) {
+			for(int j = 0; j < this.grid[i].length; j++) {
+				if (this.grid[i][j] == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public boolean hasWon(int playerToken) {
+		// Checking for Horizontal wins
+		for(int i = 0; i < this.grid.length; i++) {
+			int inARow = 0;
+			for(int j = 0; j < this.grid[i].length; j++) {
+				if (this.grid[i][j] == playerToken) {
+					inARow++;
+					if (inARow == this.maxLengthOfWin) {
+						return true;
+					}
+				} else {
+					inARow = 0;
+				}
+			}
+		}
+		// Checking for Vertical wins
+		for(int j = 0; j < this.grid[0].length; j++) {
+			int inARow = 0;
+			for(int i = 0; i < this.grid.length; i++) {
+				if (this.grid[i][j] == playerToken) {
+					inARow++;
+					if (inARow == this.maxLengthOfWin) {
+						return true;
+					}
+				} else {
+					inARow = 0;
+				}
+			}
+		}
+		// TODO: Checking for Diagonal (from down to up) wins
+		// TODO: Checking for Diagonal (from up to down) wins
+		return false;
 	}
 	
 	public void makeMove(int row, int playerToken) {
