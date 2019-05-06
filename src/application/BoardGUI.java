31229f82763a87b6a8e100c128c9b525ee46f033
Grant Gapinski <gruntygap@gmail.com>
2019-05-06 11:33:17 -0500

    HEAVY REFACTOR OF BoardGUI

--- src/application/BoardGUI.java
+++ src/application/BoardGUI.java
@@ -20,63 +20,63 @@ import javafx.scene.paint.Color;
  */
 
 public class BoardGUI extends BorderPane {
-
-	private int columns = 7;
 	
-	private int rows = 6;
+	private GridPane gp;
 	
-	private int turn = 1;
+	private ToolBar tb;
+	
+	private Button reset;
+	
+	private Label currentPlayer;
+	
+	private GameModel model;
+	
+	private boolean ai;
 	
 	public BoardGUI() {
-		GridPane gp = new GridPane();
+		// Create objects the game is dependent on.
+		this.model = new GameModel(6, 7);
+		this.ai = true;
 		
-		for (int i = 0; i < columns; i ++) {
+		// Configure GridPane
+		this.gp = new GridPane();
+		gp.getStyleClass().add("game-board");
+		initGridPane();
+		
+		// Configure ToolBar
+		this.tb = new ToolBar();
+		this.reset = new Button("Reset");
+		reset.setOnAction((event) -> {
+			try {
+				model = new GameModel(6, 7);
+			} catch (Exception e) {
+				// Add alert?
+			}
+		});
+		this.currentPlayer = new Label("Current Turn: " + model.getTurn());
+		tb.getItems().addAll(reset,currentPlayer);
+		
+		// Adds items this.BoardGUI which is a BorderPane
+		this.setTop(tb);
+		this.setCenter(gp);	
+	}
+	
+	private void initGridPane() {
+		int[][] grid = model.getGrid();
+		for (int i = 0; i < grid[0].length; i ++) {
 			ColumnConstraints column = new ColumnConstraints();
 			gp.getColumnConstraints().add(column);
 		}
-		
-		for (int i = 0; i < rows; i ++) {
+		for (int i = 0; i < grid.length; i ++) {
 			RowConstraints row = new RowConstraints();
 			gp.getRowConstraints().add(row);
 		}
 		
-		ToolBar tb = new ToolBar();
-		
-		Button reset = new Button("Reset");
-		
-		Label currentPlayer = new Label("Current Turn: " + turn);
-		
-		tb.getItems().addAll(reset,currentPlayer);
-		
-		this.setTop(tb);
-		
-		for (int i = 0; i < columns; i ++) {
-			for (int j = 0; j < rows; j ++) {
+		for (int i = 0; i < grid[0].length; i ++) {
+			for (int j = 0; j < grid.length; j ++) {
 				Pane pane = new Pane();
 				pane.setMinSize(25,25);
 				pane.setPrefSize(300, 300);
-				
-				pane.setOnMouseReleased(e -> {
-					// update model by placing piece in spot
-					
-					// switches turn
-					currentPlayer.setText("Current turn: " + turn);
-					if (turn == 1) {
-						pane.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
-						turn = 2;
-					} else {
-						pane.setBackground(new Background(new BackgroundFill(Color.DARKRED, null, null)));
-						turn = 1;
-						
-					}
-					
-					// check for a winner
-					// if winner then display
-//					Alert winnerAlert = new Alert(AlertType.INFORMATION);
-//					winnerAlert.setTitle("Game Over!");
-//					winnerAlert.setContentText("Player" + "INSERT VALUE" + " won!");
-//					winnerAlert.showAndWait();
-				});
 				
 				pane.getStyleClass().add("game-cell");
 				gp.add(pane, i, j);
@@ -84,13 +84,31 @@ public class BoardGUI extends BorderPane {
 				GridPane.setVgrow(pane, Priority.ALWAYS);
 			}
 		}
-		
-		
-		gp.getStyleClass().add("game-board");
-		
-		this.setCenter(gp);
-		
-		
+//		if (turn == 1) {
+//			pane.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
+//			turn = 2;
+//		} else {
+//			pane.setBackground(new Background(new BackgroundFill(Color.DARKRED, null, null)));
+//			turn = 1;
+//			
+//		}
+//		pane.setOnMouseReleased(e -> {
+//			// 
+//			
+//			
+//			
+//			// check for a winner
+//			// if winner then display
+////			Alert winnerAlert = new Alert(AlertType.INFORMATION);
+////			winnerAlert.setTitle("Game Over!");
+////			winnerAlert.setContentText("Player" + "INSERT VALUE" + " won!");
+////			winnerAlert.showAndWait();
+//		});
+	}
+	
+	private void updateGUI() {
+		// Update Turn Text
+		currentPlayer.setText("Current turn: " + model.getTurn());
 	}
 
 }
