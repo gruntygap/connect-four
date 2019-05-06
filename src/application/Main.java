bf85974783bbe677a9caa7f6de639d73420b2725
BAILEY MIDDENDORF <baileymiddendorf@heri200h-233-149.vlan.net.its.bethel.edu>
2019-05-05 19:54:51 -0500

    moved board to its own class

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,18 +21,10 @@ import javafx.scene.paint.Color;
 
 public class Main extends Application {
 	
-	private int columns = 7;
-	
-	private int rows = 6;
-	
-	private int turn = 1;
-	
-	
-	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			boardGUI root = new boardGUI();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -52,67 +44,7 @@ public class Main extends Application {
 //			System.out.println(game);
 //			System.out.println(game.hasWon(1));
 //			System.out.println(game);
-			GridPane gp = new GridPane();
 			
-			for (int i = 0; i < columns; i ++) {
-				ColumnConstraints column = new ColumnConstraints();
-				gp.getColumnConstraints().add(column);
-			}
-			
-			for (int i = 0; i < rows; i ++) {
-				RowConstraints row = new RowConstraints();
-				gp.getRowConstraints().add(row);
-			}
-			
-			ToolBar tb = new ToolBar();
-			
-			Button reset = new Button("Reset");
-			
-			Label currentPlayer = new Label("Current Turn: " + turn);
-			
-			tb.getItems().addAll(reset,currentPlayer);
-			
-			root.setTop(tb);
-			
-			for (int i = 0; i < columns; i ++) {
-				for (int j = 0; j < rows; j ++) {
-					Pane pane = new Pane();
-					pane.setMinSize(25,25);
-					pane.setPrefSize(300, 300);
-					
-					pane.setOnMouseReleased(e -> {
-						// update model by placing piece in spot
-						
-						// switches turn
-						currentPlayer.setText("Current turn: " + turn);
-						if (turn == 1) {
-							pane.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
-							turn = 2;
-						} else {
-							pane.setBackground(new Background(new BackgroundFill(Color.DARKRED, null, null)));
-							turn = 1;
-							
-						}
-						
-						// check for a winner
-						// if winner then display
-//						Alert winnerAlert = new Alert(AlertType.INFORMATION);
-//						winnerAlert.setTitle("Game Over!");
-//						winnerAlert.setContentText("Player" + "INSERT VALUE" + " won!");
-//						winnerAlert.showAndWait();
-					});
-					
-					pane.getStyleClass().add("game-cell");
-					gp.add(pane, i, j);
-					gp.setHgrow(pane, Priority.ALWAYS);
-					gp.setVgrow(pane, Priority.ALWAYS);
-				}
-			}
-			
-			
-			gp.getStyleClass().add("game-board");
-			
-			root.setCenter(gp);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
